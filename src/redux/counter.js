b94fcf50bd87b06a2167ec4175cf0d7d0b44600e
ayshasinara <ayshasinara@gmail.com>
2023-01-18 16:55:28 +0530

    counter done

--- src/redux/counter.js
+++ src/redux/counter.js
@@ -25,7 +25,7 @@ export const counterSlice = createSlice({
     // },
 
     increamentByAmount: (state, action) => {
-      state.value = action.payload;
+      state.value += action.payload;
     },
   },
 });
